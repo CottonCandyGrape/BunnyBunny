a7555c104b45253cee0fbc7e6528de1b98d44337
JeongHo16 <cup33c@naver.com>
2024-02-14 12:34:05 +0900

    몬스터 스폰 위치 좀 띄우기

--- Assets/_Scripts/MonGenerator.cs
+++ Assets/_Scripts/MonGenerator.cs
@@ -28,7 +28,7 @@ public class MonGenerator : MonoBehaviour
     Vector3 GetMonSpawnPos()
     {
         Vector3 pos = Vector3.one;
-        float radius = Random.Range(3.0f, 5.0f);
+        float radius = Random.Range(4.0f, 6.0f);
         float sinDeg = Random.Range(0f, 360f);
         float cosDeg = Random.Range(0f, 360f);
 
