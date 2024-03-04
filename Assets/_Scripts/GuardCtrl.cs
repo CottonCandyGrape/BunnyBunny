492183450ca1e7add3373f554041a936345b593e
JeongHo16 <cup33c@naver.com>
2024-03-04 18:55:04 +0900

    spinSpeed 수정

--- Assets/_Scripts/GuardCtrl.cs
+++ Assets/_Scripts/GuardCtrl.cs
@@ -15,7 +15,7 @@ public class GuardCtrl : MonoBehaviour
     }
 
     float rotSpeed = 150.0f; //player 주변 회전 속도
-    float spinSpeed = 10.0f; //Guard 회전 속도
+    float spinSpeed = 100.0f; //Guard 회전 속도
     float radius = 1.0f;
     Vector2 center = Vector2.zero;
     Vector2 guardPos = Vector2.zero;
@@ -42,6 +42,6 @@ public class GuardCtrl : MonoBehaviour
 
         transform.position = center + guardPos * radius;
 
-        transform.Rotate(Vector3.forward, spinSpeed);
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
     }
 }
