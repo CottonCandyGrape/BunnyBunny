db821b20f174d368c2b73f9a90c004618a5f76b5
JeongHo16 <cup33c@naver.com>
2024-03-08 18:28:48 +0900

    gun levelup 기능 추가

--- Assets/_Scripts/GunCtrl.cs
+++ Assets/_Scripts/GunCtrl.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class GunCtrl : Weapon
 {
     const float BulletOffset = 0.3f;
-    const float YOffset = 0.25f;
+    const float ShotOffset = 0.25f;
     const int BaseShotCnt = 3;
-    const int LastShotCnt = 5;
     int curCount = 0;
 
     void Start() { }
@@ -16,15 +15,54 @@ public class GunCtrl : Weapon
 
     public void FireBullet(Vector3 bltDir) //방향, 위치, 각도 정하여 발사
     {
-        BulletCtrl bltCtrl = MemoryPoolMgr.Inst.AddBulletPool();
-        bltCtrl.gameObject.SetActive(true);
+        if (BaseShotCnt < curCount)
+        {
+            curCount = 0;
+            return;
+        }
 
-        bltDir.Normalize();
-        bltCtrl.MoveDir = bltDir;
+        int count = GetBulletCount();
 
-        float angle = Mathf.Atan2(bltDir.y, bltDir.x) * Mathf.Rad2Deg;
-        bltCtrl.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        bltCtrl.transform.position = transform.position + bltDir * BulletOffset;
+        for (int i = 0; i < count; i++)
+        {
+            BulletCtrl bltCtrl = MemoryPoolMgr.Inst.AddBulletPool();
+            bltCtrl.gameObject.SetActive(true);
+
+            bltDir.Normalize();
+            bltCtrl.MoveDir = bltDir;
+
+            float angle = Mathf.Atan2(bltDir.y, bltDir.x) * Mathf.Rad2Deg;
+            bltCtrl.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            bltCtrl.transform.position = GetFirePos(i, bltDir) + bltDir * BulletOffset;
+        }
+
+        curCount++;
+    }
+
+    int GetBulletCount()
+    {
+        int count = 0;
+        if (curCount < BaseShotCnt)
+            count = curLevel + 1;
+        else if (curCount == BaseShotCnt)
+            count = curLevel + 3;
+
+        return count;
+    }
+
+    Vector3 GetFirePos(int idx, Vector3 dir)
+    {
+        Vector3 firePos = GameMgr.Inst.player.transform.position;
+        Vector3 rDir = Quaternion.AngleAxis(90, Vector3.forward) * dir; //dir을 왼쪽으로 90도 돌려야함
+
+        if (curCount < BaseShotCnt)
+            firePos += rDir * (ShotOffset / 2.0f) * curLevel;
+        else if (BaseShotCnt == curCount)
+            firePos += rDir * (ShotOffset / 2.0f) * (curLevel + 2);
+
+        firePos -= rDir * ShotOffset * idx;
+
+        return firePos;
     }
 
     public void FanFire(Vector3 midDir) //부채꼴 모양 발사 스킬
@@ -39,7 +77,9 @@ public class GunCtrl : Weapon
 
     public override void LevelUpWeapon()
     {
+        if (MaxLevel <= curLevel) return;
 
+        curLevel++;
     }
 
     public override void EvolveWeapon()
