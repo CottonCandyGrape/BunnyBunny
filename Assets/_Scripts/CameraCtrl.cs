6982684253cd146fbb080de7bc3dd33518fc0c54
JeongHo16 <cup33c@naver.com>
2024-03-15 17:34:47 +0900

    보스전 zoomout 코드 추가

--- Assets/_Scripts/CameraCtrl.cs
+++ Assets/_Scripts/CameraCtrl.cs
@@ -6,9 +6,12 @@ public class CameraCtrl : MonoBehaviour
 {
     Transform player = null;
     Vector3 playerPos = Vector3.zero;
+    Camera cam = null;
 
     void Start()
     {
+        cam = Camera.main;
+
         if (!player)
             player = GameObject.Find("PlayerRoot").GetComponent<Transform>();
     }
@@ -24,5 +27,25 @@ public class CameraCtrl : MonoBehaviour
         playerPos = player.position;
         playerPos.z = transform.position.z;
         transform.position = playerPos;
+    }
+
+    public void ZoomOut()
+    {
+        StartCoroutine(ZoomOutCo());
+    }
+
+    IEnumerator ZoomOutCo()
+    {
+        float size = 7.0f;
+        float zTimer = 0.0f;
+        float zTime = 1.0f;
+        float speed = 0.5f; //TODO : 추후 조정
+
+        while (zTimer <= 1.0f)
+        {
+            zTimer += speed * Time.deltaTime;
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, size, (zTimer / zTime));
+            yield return null;
+        }
     }
 }
