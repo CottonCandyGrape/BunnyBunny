d0508cf8e216a54b645ca0d788be53127b006058
JeongHo16 <cup33c@naver.com>
2024-03-23 01:26:53 +0900

    행동패턴 만드는 중

--- Assets/_Scripts/MeatSoldierCtrl.cs
+++ Assets/_Scripts/MeatSoldierCtrl.cs
@@ -2,26 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ActionState { None, Walk, Run, Shot } //일단 Run까지만 구현하자
+
 public class MeatSoldierCtrl : MonsterCtrl
 {
+    public ActionState actState = ActionState.None;
+
     float bossHp = 1000;
     float collDmg = 30;
+    float walkTimer = 0.0f;
+    float walkTime = 5.0f;
+
+    //float runWaitTimer = 0.0f; 
+    //float runWaitTime = 0.5f;
+
+    bool isDie = false;
+
+    //public bool isRun = false;
+
+    CapsuleCollider2D capColl = null;
+
+    //WaitForSeconds waitRunSec = new WaitForSeconds(0.5f);
+    //Coroutine runCo = null;
 
     void Start()
     {
+        capColl = GetComponent<CapsuleCollider2D>();
+
         InitBoss();
     }
 
-    //void FixedUpdate() { } 
+    void FixedUpdate()
+    {
+        if (!GameMgr.Inst.hasBoss) return; //깜빡일때 안움직이기
+
+        if (actState == ActionState.Walk)
+            base.Move();
+        //else if (actState == ActionState.Run) //TODO :
+        //{
+        //    if (runCo == null && !isRun)
+        //        runCo = StartCoroutine(RunAction());
+        //}
+        //else if (actState == ActionState.Shot)
+        // TODO : 행동 패턴 만들기
+    }
 
     void Update()
     {
-        //base.Move(); //TODO : 얘도 Collision이 생기니깐 fixed에서 할까?
+        UpdateActionState();
     }
 
     protected override void OnTriggerEnter2D(Collider2D coll)
     {
-        if (!GameMgr.Inst.hasBoss) return; // 깜빡거릴때는 안맞기
+        if (!GameMgr.Inst.hasBoss) return; // 깜빡일때 안맞기
 
         base.OnTriggerEnter2D(coll);
     }
@@ -39,8 +72,87 @@ public class MeatSoldierCtrl : MonsterCtrl
     {
         monType = MonsterType.BossMon;
         curHp = bossHp;
-        StartCoroutine(BlinkBoss());
+        capColl.enabled = false;
+
+        runCo = StartCoroutine(BlinkBoss());
     }
+
+    void UpdateActionState()
+    {
+        if (isDie) return; //죽으면 return
+
+        if (!GameMgr.Inst.hasBoss) return; //깜빡일 땐 return 
+
+        if (actState == ActionState.None)
+        {
+            actState = ActionState.Walk;
+            walkTimer = 5.0f;
+        }
+
+        switch (actState)
+        {
+            case ActionState.Walk:
+                {
+                    walkTimer -= Time.deltaTime;
+                    if (walkTimer < 0.0f)
+                    {
+                        walkTimer = walkTime;
+
+                        actState = ActionState.Run;
+                        animator.SetBool("Run", true);
+                    }
+                    break;
+                }
+
+            case ActionState.Run:
+                {
+                    //TODO : 3번 추격 끝나고 나서 상태 바꾸기
+                    //if (runCo != null && !isRun)
+                    //{
+                    //    runCo = null;
+                    //    //actState = ActionState.Shot;
+                    //    actState = ActionState.Walk;
+                    //    animator.SetBool("Run", false);
+                    //}
+                    break;
+                }
+
+                //case ActionState.Shot: //TODO : 보류 
+                //    {
+                //        actState = ActionState.Walk;
+                //        break;
+                //    }
+        }
+    }
+
+    //IEnumerator RunAction() //TODO 
+    //{
+    //    int dashCount = 3;
+    //    moveSpeed = 3.0f;
+    //    isRun = true;
+
+    //    for (int i = 0; i < dashCount; i++)
+    //    {
+    //        yield return waitRunSec;
+
+    //        //TODO : flip 도 해야함
+    //        Vector3 target = GameMgr.Inst.player.transform.position;
+    //        moveDir = target - transform.position;
+    //        moveDir.Normalize();
+
+    //        float lVal = 0.0f;
+    //        while (lVal <= 1.0f)
+    //        {
+    //            yield return null;
+    //            lVal += Time.deltaTime;
+    //            //rigid.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+    //            rigid.MovePosition(transform.position + moveDir * Time.deltaTime);
+    //            //transform.position = Vector3.Lerp(transform.position, target, lVal);
+    //        }
+    //    }
+
+    //    isRun = false;
+    //}
 
     public override void TakeDamage(float damage)
     {
@@ -52,6 +164,7 @@ public class MeatSoldierCtrl : MonsterCtrl
 
     protected override void MonsterDie()
     {
+        isDie = true;
         GameMgr.Inst.GameOver();
     }
 
@@ -78,6 +191,14 @@ public class MeatSoldierCtrl : MonsterCtrl
             }
         }
 
+        BattleSetting();
+        if (runCo != null)
+            runCo = null;
+    }
+
+    void BattleSetting()
+    {
         GameMgr.Inst.hasBoss = true;
+        capColl.enabled = true;
     }
 }
