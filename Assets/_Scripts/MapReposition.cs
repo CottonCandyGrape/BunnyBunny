7bd02561b21f0c01a020bd0b1f46f3c5ec674171
JeongHo16 <cup33c@naver.com>
2024-03-25 14:23:54 +0900

    무한맵 버그 수정

--- Assets/_Scripts/MapReposition.cs
+++ Assets/_Scripts/MapReposition.cs
@@ -6,21 +6,35 @@ public class MapReposition : MonoBehaviour
 {
     int moveDist = 40;
 
+    float dirX = 0.0f;
+    float dirY = 0.0f;
+    float distX = 0.0f;
+    float distY = 0.0f;
+    float distXY = 0.0f;
+
     void OnTriggerExit2D(Collider2D coll)
     {
-        if (!coll.CompareTag("Area"))
-            return;
+        if (!coll.CompareTag("Area")) return;
 
         Vector3 playerPos = GameMgr.Inst.player.transform.position;
         Vector3 tilePos = this.transform.position;
 
-        float distX = Mathf.Abs(playerPos.x - tilePos.x);
-        float distY = Mathf.Abs(playerPos.y - tilePos.y);
+        dirX = playerPos.x - tilePos.x;
+        dirY = playerPos.y - tilePos.y;
 
-        float dirX = GameMgr.Inst.player.h < 0 ? -1 : 1;
-        float dirY = GameMgr.Inst.player.v < 0 ? -1 : 1;
+        distX = Mathf.Abs(dirX);
+        distY = Mathf.Abs(dirY);
+        distXY = Mathf.Abs(distX - distY);
 
-        if (distX > distY)
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        if (distXY <= 0.1f)
+        {
+            transform.position += Vector3.up * dirY * moveDist;
+            transform.position += Vector3.right * dirX * moveDist;
+        }
+        else if (distX > distY)
             transform.position += Vector3.right * dirX * moveDist;
         else if (distX < distY)
             transform.position += Vector3.up * dirY * moveDist;
