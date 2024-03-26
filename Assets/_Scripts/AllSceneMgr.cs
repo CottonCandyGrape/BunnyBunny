6f9021ef08cd129411dc06f8eb38b2fa53235a9d
JeongHo16 <cup33c@naver.com>
2024-03-26 20:47:21 +0900

    singleton, BattleSceneMgr script 추가

---
+++ Assets/_Scripts/AllSceneMgr.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllSceneMgr : G_Singleton<AllSceneMgr> 
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
