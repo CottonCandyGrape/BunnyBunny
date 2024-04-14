d65b5b3cccf4ce50e21bb9e9d139d63895762714
JeongHo16 <cup33c@naver.com>
2024-04-14 18:27:12 +0900

    BombEffect -> AnimEffect 이름 변경

--- Assets/_Scripts/AnimEffect.cs
+++ Assets/_Scripts/AnimEffect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombEffect : MonoBehaviour
+public class AnimEffect : MonoBehaviour
 {
     Animator anim = null;
 
